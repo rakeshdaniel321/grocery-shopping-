328b1b48253f5bc4b6aeaf86c581240df4a949d3
rakeshdaniel <rakeshdaniel321@gmail.com>
2026-01-04 18:03:22 +0530

    Login state build

--- grocery/frontend/grocery/src/component/Login.jsx
+++ grocery/frontend/grocery/src/component/Login.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 
 function Login (){
+
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    const [error,setError]=useState('');
+    const [loading,setLoading]=useState(false);
+
+    const handleLogin = async ()=>{
+        if(!email || !password){
+            setError(" All fields are required");
+            return;
+        }
+        try {
+            
+            
+            
+        } catch (error) {
+            
+        }
+    }
+
     return(
-        <div>hi login page </div>
+        <div>
+
+            <input type="email" placeholder="Email" value={email}
+            onChange={(e)=>setEmail(e.target.value)}/>
+
+              <input type="password" placeholder="Password" value={password}
+            onChange={(e)=>setPassword(e.target.value)}/>
+
+             {error && <p style={{ color: "red" }}>{error}</p>}
+
+
+
+        </div>
     );
 };
